7ef3aa052c21cd79286bb0aff9bc14fb3f1a6f4f
Mikhail Piatrenka <mikhail_piatrenka@epam.com>
2020-04-20 22:52:46 +0300

    google cloud home page

--- Module_7/src/test/java/cloud_google/page/GoogleCloudHomePage.java
+++ Module_7/src/test/java/cloud_google/page/GoogleCloudHomePage.java
@@ -1,4 +1,15 @@
 package cloud_google.page;
 
+import org.openqa.selenium.WebDriver;
+
 public class GoogleCloudHomePage {
+
+    private static final String HOMEPAGE_URL = "https://cloud.google.com/";
+    private WebDriver driver;
+
+    public GoogleCloudHomePage(WebDriver driver){
+        this.driver = driver;
+
+    }
+
 }
